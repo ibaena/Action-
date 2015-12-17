8306ec8ac53d9c61e60d625b08f7e941c39069e0
Ivan <ivan.baena04@gmail.com>
2015-12-17 04:49:20 -0500

    expramental branch try new api

--- js/main.js
+++ js/main.js
@@ -5,28 +5,31 @@ function searchCatalog(){
   $("#search-btn").on('click',function(){
   var input = $('#search-input').val();
   var omdbUrl = 'http://www.omdbapi.com/?s='+input+'&format=json';
+  var omdbPlot = 'http://www.omdbapi.com/?t='+input+'&format=json';
   var images;
+  var plot;
   
-
-
     $.getJSON(omdbUrl, function(data){ 
       images = data.Search;
       var grid = " ";
+      var title = " ";
 
       for (var i = 0; i < images.length; i++) {
         console.log(images[i].Poster);
         grid+='<div class="col s3"><img class="responsive-img" src='+images[i].Poster+' /></div>';
-      };
-      
-      $('.content').html(grid);
 
-      
+      };
+      //Movie Grid
+      $('.content').html(grid);
       });
-         //Movie Grid
-   
-     
-    })
-  }
+
+    $.getJSON(omdbPlot, function(data){
+      plot = data.Title;
+      console.log(data.Plot);
+
+    });
+  })
+}
 
 
 searchCatalog();
