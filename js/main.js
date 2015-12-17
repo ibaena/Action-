3c820dfba4628cae3cbc148cda1dccb03f2d8831
Ivan <ivan.baena04@gmail.com>
2015-12-17 01:18:01 -0500

    working on search bar and ajax content updating and removing

--- js/main.js
+++ js/main.js
@@ -1,23 +1,39 @@
 $(document).ready(function(){
 
 
-//ajax OMDB
-$("#search-btn").on('click',function(){
+
+//ajax OMDB Search Movies
+function searchCatalog(){
+  $("#search-btn").on('click',function(){
   var input = $('#search-input').val();
   var omdbUrl = 'http://www.omdbapi.com/?s='+input+'&format=json';
+  $( "input" ).submit();
+
 
     $.getJSON(omdbUrl, function(data){ 
       images = data.Search;
       for (var i = 0; i < images.length; i++) {
         console.log(data.Search[i].Poster);
+
       //Movie Grid
-        $('.images').append('<img src='+data.Search[i].Poster+'/>')
-        $('.images').append('<h3>'+data.Search[i].Title+'</h3>')
-
-    };
-
+        $('.image').append('<div class="col s3"><img class="responsive-img" src='+data.Search[i].Poster+'/></div>');
+      };
     })
+  })
+}
 
-})
+searchCatalog();
+resetSearch();
+
+
+//submit button reset ajax portion
+function resetSearch(){
+  $( "#remove-btn").bind( "click",function(){
+  $('.image').hide();
+  searchCatalog();searchCatalog();
+  });
+}
+
+
 
 });
