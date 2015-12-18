d1d9a29440a47b3399d2421ce09ef9cc91d7b230
Ivan <ivan.baena04@gmail.com>
2015-12-17 19:56:58 -0500

    pulling plot json

--- js/main.js
+++ js/main.js
@@ -4,30 +4,32 @@ $(document).ready(function(){
 function searchCatalog(){
   $("#search-input").keyup(function(){
   var input = $('#search-input').val();
-  var omdbUrl = 'http://www.omdbapi.com/?s='+input+'&plot=full&format=json';
+  var omdbUrl = 'http://www.omdbapi.com/?s='+input+'&format=json';
+  var omdbPlot = 'http://www.omdbapi.com/?t='+input+'&format=json';
   var images;
+  var details;
   
-
-
     $.getJSON(omdbUrl, function(data){ 
       images = data.Search;
       var grid = " ";
+      var title = " ";
 
       for (var i = 0; i < images.length; i++) {
-        // console.log(images[i].Poster);
-        var title = images[i].Title;
-        grid+='<div class="col s3"><img class="responsive-img" src='+images[i].Poster+' /></div>';
-      };
-      
-      $('.content').html(grid);
+        console.log(images[i].Poster);
+        grid+='<div class="col s3"><a href="http://www.omdbapi.com/?t='+images[i].Title+'&format=json" src=""><img class="responsive-img" src='+images[i].Poster+' /></a></div>';
 
-      
+      };
+      //Movie Grid
+      $('.content').html(grid);
       });
-         //Movie Grid
-   
-     
-    })
-  }
+
+    $.getJSON(omdbPlot, function(data){
+      
+      console.log(data.Plot);
+
+    });
+  })
+}
 
 
 searchCatalog();
