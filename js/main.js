1146e2dccb1d6287363c6448b4d3111c5dfff8c9
Ivan <ivan.baena04@gmail.com>
2015-12-18 04:26:32 -0500

    switch to tmdb api better documentation

--- js/main.js
+++ js/main.js
@@ -1,39 +1,56 @@
 $(document).ready(function(){
 
-//ajax OMDB Search Movies
-function searchCatalog(){
-  $("#search-input").keyup(function(){
-  var input = $('#search-input').val();
-  var omdbUrl = 'http://www.omdbapi.com/?s='+input+'&format=json';
-  var omdbPlot = 'http://www.omdbapi.com/?t='+input+'&format=json';
-  var images;
-  var details;
-  
-    $.getJSON(omdbUrl, function(data){ 
-      images = data.Search;
+
+
+//tmdb
+function tmdbApi(){
+  $('#search-input').keypress(function(e){
+      var input = $('#search-input').val();
+      var plotUrl= 'http://api.themoviedb.org/3/search/multi?api_key=3729ffa22dfa780e9abb43dee3074695&query='+input;
+      var movie;
       var grid = " ";
-      var title = " ";
 
-      for (var i = 0; i < images.length; i++) {
-        console.log(images[i].Poster);
-        grid+='<div class="col s3"><a href="http://www.omdbapi.com/?t='+images[i].Title+'&format=json" src=""><img class="responsive-img" src='+images[i].Poster+' /></a></div>';
 
-      };
-      //Movie Grid
-      $('.content').html(grid);
-      });
-
-    $.getJSON(omdbPlot, function(data){
+    if(e.which == 13) {
+    $.getJSON(plotUrl, function(data){
+      movie = data.results;
       
-      console.log(data.Plot);
+      
 
-    });
+      for (var i = 0; i < movie.length; i++) {
+        grid+='<div class="col s3"><img class="responsive-img poster" src="http://image.tmdb.org/t/p/w500'+movie[i].poster_path+'" /></div>';
+        console.log(movie[i]);
+      };
+
+      $('.content').html(grid);
+
+      
+    })
+}
+  })
+
+}
+tmdbApi();
+
+
+//Movie Genre
+function genreList(){
+  var genreList = 'https://api.themoviedb.org/3/genre/movie/list?api_key=3729ffa22dfa780e9abb43dee3074695';
+  var genre;
+  var genreContent = " ";
+  $('#searchDropdown').on('click', function(){
+
+    $.getJSON(genreList, function(data){
+      console.log('data');
+      genre = data.genres;
+
+      for (var i = 0; i < genre.length; i++) {
+        console.log(genre[i].name);
+        genreContent += ' <li><a href="#!" class="genre-list">'+genre[i].name+'</a></li>';
+      };
+      $('#dropdown1').html(genreContent);
+    })
   })
 }
-
-
-searchCatalog();
-
-
-
+genreList();
 });
