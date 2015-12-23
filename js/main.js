164f1eca5048d7248680b0548d23dc85c00c96ba
Ivan <ivan.baena04@gmail.com>
2015-12-23 04:39:00 -0500

    recreating html stucture in javascript editing cards for more information to display

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 
 var discoverContent;
 var releaseContent = " ";
- var genreContent = " ";
+var genreContent = " ";
+var tvList = " ";
+var discoverImage = 'https://image.tmdb.org/t/p/w500';
 
 $(document).ready(function() {
-  
-  var discoverImage = 'https://image.tmdb.org/t/p/w500';
+
+//Load Genre List
+   
 
 
 
@@ -38,27 +41,44 @@ $(document).ready(function() {
   tmdbApi();
 
 
-  //Movie Genre
+  //Will Pull Lst of Popular tv shows to go though
   function televisonList() {
-    var genreList = 'https://api.themoviedb.org/3/discover/tv?api_key=3729ffa22dfa780e9abb43dee3074695&page=';
+    var genreList = 'https://api.themoviedb.org/3/discover/tv?api_key=3729ffa22dfa780e9abb43dee3074695&sort_by=popularity.desc&with_genre=&page='
     var page = 1;
     var genre;
+    var genreMinfo = " ";
 
     $('#searchDropdown').on('click', function() {
 
       $.getJSON(genreList, function(data) {
-        console.log('data');
+        //console.log('data');
         genre = data.results;
-
-
+        console.log(genre);
 
         for (var i = 0; i < genre.length; i++) {
-          // console.log(genre[i].name);
-          genreContent += '<div class="col s6"><div class="card"><div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" /><span class="card-title">' + genre[i].name + '</span></div></div></div>';
+          //console.log(genre[i].name);
+
+
+          genreContent += '<div class="col s6">\
+                              <div class="card">\
+                                <div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
+                                </div>\
+                              </div>\
+                              <div>\
+                                  <p class="card-title">' + genre[i].name +'</p>\
+                                  <span class="tv-plot right align">'+genre[i].vote_average+'<i class="tiny material-icons">grade</i></span>\
+                              </div>\
+                              <div>\
+                                  <p class="card-title">' + genre[i].genre_ids+'</p>\
+                                  <span class="tv-plot right align">'+genre[i].first_air_date+'<i class="tiny material-icons">grade</i></span>\
+                              </div>\
+                          </div>';
+        
         }
         $('.content').html(genreContent).hide().fadeIn(400);
         $('#previous-btn').html('<a class="waves-effect waves-black btn-flat" id="popular-backbtn"><i class="material-icons">skip_previous</i></a>');
         $('#next-btn').html('<a class="waves-effect waves-black btn-flat" id="popular-addbtn"><i class="material-icons">skip_next</i></a>');
+
       });
     });
 
@@ -74,7 +94,14 @@ $(document).ready(function() {
 
         for (var i = 0; i < genre.length; i++) {
 
-          genreContent += '<div class="col s6"><div class="card"><div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" /><span class="card-title">' + genre[i].title + '</span></div></div></div>';
+           genreContent += '<div class="col s6">\
+                              <div class="card">\
+                                <div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
+                                </div>\
+                                <span class="card-title">' + genre[i].name +'</span>\
+                                <span class="tv-plot">'+genre[i].vote_average+'<i class="tiny material-icons">grade</i></span>\
+                              </div>\
+                          </div>';
         }
         $('.content').html(genreContent).hide().fadeIn(400);
 
@@ -95,7 +122,14 @@ $(document).ready(function() {
 
         for (var i = 0; i < genre.length; i++) {
 
-          genreContent += '<div class="col s6"><div class="card"><div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" /><span class="card-title">' + genre[i].title + '</span></div></div></div>';
+          genreContent += '<div class="col s6">\
+                              <div class="card">\
+                                <div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
+                                </div>\
+                                <span class="card-title">' + genre[i].name +'</span>\
+                                <span class="tv-plot">'+genre[i].vote_average+'<i class="tiny material-icons">grade</i></span>\
+                              </div>\
+                          </div>';
         }
         $('.content').html(genreContent).hide().fadeIn(400);
       });
@@ -143,10 +177,11 @@ $(document).ready(function() {
 
       $.getJSON(discoverUrl, function(data) {
         discover = data.results;
-
+        console
+       
         for (var i = 0; i < discover.length; i++) {
           if (typeof(discoverContent) != 'undefined') {
-            discoverContent += '<div class="col s6"><div class="card"><div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + discover[i].backdrop_path + '" /><span class="card-title">' + discover[i].title + '</span></div></div></div>';
+            discoverContent += '<div class="col s6"><div class="card"><div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + discover[i].backdrop_path + '" /><span class="card-title">' + discover[i].title + '</span><p>Score:<p>/div></div></div>';
           } else {
             discoverContent = '<div class="col s6"><div class="card"><div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + discover[i].backdrop_path + '" /><span class="card-title">' + discover[i].title + '</span></div></div></div>';
           }
@@ -200,4 +235,6 @@ $(document).ready(function() {
 
 
 
+
 });
+
