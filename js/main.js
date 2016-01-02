7488bf4a13fac5fd43a61a6cd749b4d86e8909fb
Ivan <ivan.baena04@gmail.com>
2016-01-02 12:33:40 -0500

    added side nav

--- js/main.js
+++ js/main.js
@@ -2,18 +2,19 @@
 var discoverContent;
 var releaseContent = " ";
 var genreContent = " ";
+var popularContent = " ";
 var tvList = " ";
 var discoverImage = 'https://image.tmdb.org/t/p/w500';
-var tvGlist;
 
 $(document).ready(function() {
+  $(".button-collapse").sideNav();
 
-  $( ".poster" )
-  .error(function() {
-    $( this ).attr( "src", "replacement.png" );
-  })
-  .attr( "src", "missing.png" );
-
+//enables coallpasible for plot descriptions
+$('.content').on('click','.collapsible-header',function(){
+$('.collapsible').collapsible({
+      accordion : false 
+    });
+});
 
   //tmdb
   function tmdbApi() {
@@ -29,7 +30,22 @@ $(document).ready(function() {
           movie = data.results;
 
           for (var i = 0; i < movie.length; i++) {
-            grid += '<div class="col s6"><div class="card"><div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + movie[i].backdrop_path + '" /><span class="card-title">' + movie[i].title + '</span></div></div></div>';
+            grid += '<div class="col s6">\
+                              <div class="card">\
+                                <div class="card-image">\
+                                  <img class="responsive-img poster" src="' + discoverImage + '' + movie[i].backdrop_path + '" />\
+                                </div>\
+                              </div>\
+                                <ul class="collapsible" data-collapsible="accordion">\
+                                  <li>\
+                                    <div class="collapsible-header">' + movie[i].name +' <span class="tv-plot right align">'+movie[i].vote_average+'<i class="tiny material-icons">grade</i></span></div>\
+                                    <div class="collapsible-body">\
+                                      <p>'+movie[i].overview+'</p>\
+                                      <p>'+movie[i].genre_ids+'</p>\
+                                    </div>\
+                                  </li>\
+                                </ul>\
+                          </div>';
             //console.log(movie[i]);
           }
 
@@ -42,101 +58,227 @@ $(document).ready(function() {
   }
   tmdbApi();
 
+  /*tv Genre List reference
+     $.getJSON('https://api.themoviedb.org/3/genre/tv/list?api_key=3729ffa22dfa780e9abb43dee3074695', function(data){
+            var names =  data.genres;
+            console.log(names)
 
-  //Will Pull Lst of Popular tv shows to go though
-  function televisonList() {
-    var genreList = 'https://api.themoviedb.org/3/discover/tv?api_key=3729ffa22dfa780e9abb43dee3074695&sort_by=popularity.desc&with_genre=&page=&append_to_response=similar_tv,genres,&language=DE'
-    var page = 1;
-    var genre;
-    var genreMinfo = " ";
+           });*/
 
-    $('#searchDropdown').on('click', function() {
-      $('.poster').error(function(){
-        $(this).attr('src', 'images/insta.png');
-});
+//Will Pull Lst of top rated tv shows to go though
+function topRatedtelevison() {
+  var genreList = 'https://api.themoviedb.org/3/tv/top_rated?api_key=3729ffa22dfa780e9abb43dee3074695&page=';
+  var page = 1;
+  var genre;
+  var genreMinfo = " ";
 
-      $.getJSON(genreList, function(data) {
-        //console.log('data');
-        genre = data.results;
-        console.log(genre);
+  $('#top-tv').on('click', function() {
+    $.getJSON(genreList, function(data) {
+      genre = data.results;
+      //console.log(genre);
 
-        for (var i = 0; i < genre.length; i++) {
-          //console.log(genre[i].name);
+      for (var i = 0; i < genre.length; i++) {
+        //console.log(genre[i].name);
 
-
-          genreContent += '<div class="col s6">\
+        genreContent += '<div class="col s6">\
                               <div class="card">\
-                                <div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
+                                <div class="card-image">\
+                                  <img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
                                 </div>\
                               </div>\
-                                  <p class="card-title">' + genre[i].name +'</p>\
-                                  <span class="tv-plot right align">'+genre[i].vote_average+'<i class="tiny material-icons">grade</i></span>\
+                                <ul class="collapsible" data-collapsible="accordion">\
+                                  <li>\
+                                    <div class="collapsible-header">' + genre[i].name + ' <span class="tv-plot right align">' + genre[i].vote_average + '<i class="tiny material-icons">grade</i></span></div>\
+                                    <div class="collapsible-body">\
+                                      <p>' + genre[i].overview + '</p>\
+                                      <p>' + genre[i].genre_ids + '</p>\
+                                    </div>\
+                                  </li>\
+                                </ul>\
                           </div>';
-        
-        }
-        $('.content').html(genreContent).hide().fadeIn(400);
-        $('#previous-btn').html('<a class="waves-effect waves-black btn-flat" id="popular-backbtn"><i class="material-icons">skip_previous</i></a>');
-        $('#next-btn').html('<a class="waves-effect waves-black btn-flat" id="popular-addbtn"><i class="material-icons">skip_next</i></a>');
+      }
 
-      });
-    });
+      $('.content').html(genreContent).hide().fadeIn(400);
+      $('#previous-btn').html('<a class="waves-effect waves-black btn-flat" id="toprated-backbtn"><i class="material-icons">skip_previous</i></a>');
+      $('#next-btn').html('<a class="waves-effect waves-black btn-flat" id="toprated-addbtn"><i class="material-icons">skip_next</i></a>');
 
-    //next page
-    $('#next-btn').on('click', '#popular-addbtn', function(e) {
-      e.preventDefault();
-      page = page + 1;
-      genreList = 'https://api.themoviedb.org/3/discover/tv?api_key=3729ffa22dfa780e9abb43dee3074695&page='+ page;
-      genreContent = " "
-
-      $.getJSON(genreList, function(data) {
-        genre = data.results;
-
-        for (var i = 0; i < genre.length; i++) {
-
-            genreContent += '<div class="col s6">\
-                              <div class="card">\
-                                <div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
-                                </div>\
-                              </div>\
-                                  <p class="card-title">' + genre[i].name +'</p>\
-                                  <span class="tv-plot right align">'+genre[i].vote_average+'<i class="tiny material-icons">grade</i></span>\
-                          </div>';
-        }
-        $('.content').html(genreContent).hide().fadeIn(400);
-
-
-      });
 
     });
-    //Previous Button
-    $('#previous-btn').on('click', '#popular-backbtn', function(e) {
-      e.preventDefault();
-      page = page - 1;
-      genreContent = " ";
+  });
 
-      genreUrl = 'https://api.themoviedb.org/3/discover/tv?api_key=3729ffa22dfa780e9abb43dee3074695&page=' + page;
+  //next page
+  $('#next-btn').on('click', '#toprated-addbtn', function(e) {
+    e.preventDefault();
+    page = page + 1;
+    genreList = 'https://api.themoviedb.org/3/tv/top_rated?api_key=3729ffa22dfa780e9abb43dee3074695&page=' + page;
+    genreContent = " "
 
-      $.getJSON(genreUrl, function(data) {
-        genre = data.results;
+    $.getJSON(genreList, function(data) {
+      genre = data.results;
 
-        for (var i = 0; i < genre.length; i++) {
+      for (var i = 0; i < genre.length; i++) {
 
-          genreContent += '<div class="col s6">\
+        genreContent += '<div class="col s6">\
                               <div class="card">\
-                                <div class="card-image"><img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
+                                <div class="card-image">\
+                                  <img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
                                 </div>\
                               </div>\
-                                  <p class="card-title">' + genre[i].name +'</p>\
-                                  <span class="tv-plot right align">'+genre[i].vote_average+'<i class="tiny material-icons">grade</i></span>\
+                                <ul class="collapsible" data-collapsible="accordion">\
+                                  <li>\
+                                    <div class="collapsible-header">' + genre[i].name + ' <span class="tv-plot right align">' + genre[i].vote_average + '<i class="tiny material-icons">grade</i></span></div>\
+                                    <div class="collapsible-body"><p>' + genre[i].overview + '</p></div>\
+                                  </li>\
+                                </ul>\
                           </div>';
-        }
-        $('.content').html(genreContent).hide().fadeIn(400);
-      });
+      }
+      $('.content').html(genreContent).hide().fadeIn(400);
+
 
     });
 
-  }
-  televisonList();
+  });
+  //Previous Button
+  $('#previous-btn').on('click', '#toprated-backbtn', function(e) {
+    e.preventDefault();
+    page = page - 1;
+    genreContent = " ";
+
+    genreUrl = 'https://api.themoviedb.org/3/tv/top_rated?api_key=3729ffa22dfa780e9abb43dee3074695&page=' + page;
+
+    $.getJSON(genreUrl, function(data) {
+      genre = data.results;
+
+      for (var i = 0; i < genre.length; i++) {
+
+        genreContent += '<div class="col s6">\
+                              <div class="card">\
+                                <div class="card-image">\
+                                  <img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
+                                </div>\
+                              </div>\
+                                <ul class="collapsible" data-collapsible="accordion">\
+                                  <li>\
+                                    <div class="collapsible-header">' + genre[i].name + ' <span class="tv-plot right align">' + genre[i].vote_average + '<i class="tiny material-icons">grade</i></span></div>\
+                                    <div class="collapsible-body"><p>' + genre[i].overview + '</p></div>\
+                                  </li>\
+                                </ul>\
+                          </div>';
+      }
+      $('.content').html(genreContent).hide().fadeIn(400);
+    });
+
+  });
+
+}
+topRatedtelevison();
+
+//Will Pull Lst of Popular tv shows to go though
+function popularTelevison() {
+  var popularList = 'https://api.themoviedb.org/3/tv/popular?api_key=3729ffa22dfa780e9abb43dee3074695&page=';
+  var page = 1;
+  var popular;
+
+  $('#popular-tv').on('click', function() {
+    $.getJSON(popularList, function(data) {
+      popular = data.results;
+      //console.log(genre);
+
+      for (var i = 0; i < popular.length; i++) {
+        //console.log(genre[i].name);
+
+        popularContent += '<div class="col s6">\
+                              <div class="card">\
+                                <div class="card-image">\
+                                  <img class="responsive-img poster" src="' + discoverImage + '' + popular[i].backdrop_path + '" />\
+                                </div>\
+                              </div>\
+                                <ul class="collapsible" data-collapsible="accordion">\
+                                  <li>\
+                                    <div class="collapsible-header">' + popular[i].name + ' <span class="tv-plot right align">' + popular[i].vote_average + '<i class="tiny material-icons">grade</i></span></div>\
+                                    <div class="collapsible-body">\
+                                      <p>' + popular[i].overview + '</p>\
+                                      <p>' + popular[i].genre_ids + '</p>\
+                                    </div>\
+                                  </li>\
+                                </ul>\
+                          </div>';
+      }
+
+      $('.content').html(popularContent).hide().fadeIn(400);
+      $('#previous-btn').html('<a class="waves-effect waves-black btn-flat" id="popular-backbtn"><i class="material-icons">skip_previous</i></a>');
+      $('#next-btn').html('<a class="waves-effect waves-black btn-flat" id="popular-addbtn"><i class="material-icons">skip_next</i></a>');
+
+
+    });
+  });
+
+  //next page
+  $('#next-btn').on('click', '#popular-addbtn', function(e) {
+    e.preventDefault();
+    page = page + 1;
+    genreList = 'https://api.themoviedb.org/3/tv/popular?api_key=3729ffa22dfa780e9abb43dee3074695&page=' + page;
+    genreContent = " "
+
+    $.getJSON(genreList, function(data) {
+      genre = data.results;
+
+      for (var i = 0; i < genre.length; i++) {
+
+        genreContent += '<div class="col s6">\
+                              <div class="card">\
+                                <div class="card-image">\
+                                  <img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
+                                </div>\
+                              </div>\
+                                <ul class="collapsible" data-collapsible="accordion">\
+                                  <li>\
+                                    <div class="collapsible-header">' + genre[i].name + ' <span class="tv-plot right align">' + genre[i].vote_average + '<i class="tiny material-icons">grade</i></span></div>\
+                                    <div class="collapsible-body"><p>' + genre[i].overview + '</p></div>\
+                                  </li>\
+                                </ul>\
+                          </div>';
+      }
+      $('.content').html(genreContent).hide().fadeIn(400);
+
+
+    });
+
+  });
+  //Previous Button
+  $('#previous-btn').on('click', '#popular-backbtn', function(e) {
+    e.preventDefault();
+    page = page - 1;
+    genreContent = " ";
+
+    genreUrl = 'https://api.themoviedb.org/3/tv/popular?api_key=3729ffa22dfa780e9abb43dee3074695&page=' + page;
+
+    $.getJSON(genreUrl, function(data) {
+      genre = data.results;
+
+      for (var i = 0; i < genre.length; i++) {
+
+        genreContent += '<div class="col s6">\
+                              <div class="card">\
+                                <div class="card-image">\
+                                  <img class="responsive-img poster" src="' + discoverImage + '' + genre[i].backdrop_path + '" />\
+                                </div>\
+                              </div>\
+                                <ul class="collapsible" data-collapsible="accordion">\
+                                  <li>\
+                                    <div class="collapsible-header">' + genre[i].name + ' <span class="tv-plot right align">' + genre[i].vote_average + '<i class="tiny material-icons">grade</i></span></div>\
+                                    <div class="collapsible-body"><p>' + genre[i].overview + '</p></div>\
+                                  </li>\
+                                </ul>\
+                          </div>';
+      }
+      $('.content').html(genreContent).hide().fadeIn(400);
+    });
+
+  });
+
+}
+popularTelevison();
 
   //create Theatre list
   function moviesOut() {
