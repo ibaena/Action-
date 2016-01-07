41205161977cec3672a602bdc2018017c61d3b17
Serviox19 <servio19@gmail.com>
2016-01-06 22:31:33 -0500

    successfully getting back data based on user input

--- js/showtimes.js
+++ js/showtimes.js
@@ -1,12 +1,28 @@
 $(document).ready(function () {
-  // variables for url construction
-  var apiKey = "3pc86f2jfnwqg359td2p3vwr";
-  var url = "http://data.tmsapi.com/v1.1";
-  var showtimesUrl = url + '/movies/showings';
-  var zipCode = $('#zip').val();
 
-  $.ajax({
-    url: showtimesUrl,
+  $(".input-field").keypress(function (e) {
+    // variables for url construction
+    var today =;
+    var apiKey = "3pc86f2jfnwqg359td2p3vwr";
+    var zipCode = $("#zip").val();
+    var mileEntry = $("#radius").val();
+    var showtimesUrl = "http://data.tmsapi.com/v1.1";
+    showtimesUrl += "/movies/showings?";
+    showtimesUrl += "startDate=" + "2016-01-07";
+    showtimesUrl += "&zip=" + zipCode;
+    showtimesUrl += "&radius=" + mileEntry + "&units=mi";
+    showtimesUrl += "&api_key=" + apiKey;
+    
 
-  })
+    if (e.which == 13) {
+      $.ajax({
+        type: "GET",
+        url: showtimesUrl,
+        success: (function (data) {
+          console.log(data);
+        })
+      });
+    };
+  
+  }); 
 });
